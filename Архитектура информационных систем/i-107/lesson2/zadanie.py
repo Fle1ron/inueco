c9b7b25768dc78862cb0ff7065c4e4843c62d6c8
Fle1ron <seregau444@gmail.com>
2026-02-20 11:34:30 +0500

    Юдин Евгений И-107

--- Архитектура информационных систем/i-107/lesson2/zadanie.py
+++ Архитектура информационных систем/i-107/lesson2/zadanie.py
@@ -1,21 +1,7 @@
-#Юдин Евгений Сергеевич
-#test
-#1
-print('Hello, World!')
+# Создаем список чисел от 100 до 1,000,000 с шагом 5,000
+numbers = list(range(100, 1000001, 5000))
 
-#2
-# You Know nothing, Jon Snow!
-
-#3
-print('Robert')
-print('Stannis')
-print('Renly')
-
-#4
-print('10')
-
-#5
-print('What Is dead May Never Die')
-#test
-#6
-print('81/9')
+# Выводим список на экран
+for number in numbers:
+    print(number)
+    
